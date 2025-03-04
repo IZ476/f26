74b8d26ba7685405edf2cd68594f2e5e46332aa1
Isaac <isaac.zakon@studytafensw.edu.au>
2025-03-05 09:59:09 +1100

    bruh

--- script.js
+++ script.js
@@ -12,3 +12,35 @@
 //     }
 // }
 
+// let score = 70;
+
+// if (score > 100 || score < 0) {
+//     console.log("Invalid score");
+// } else if (score >= 90) {
+//     console.log("A");
+// } else if (score >= 80) {
+//     console.log("B");
+// } else if (score >= 70) {
+//     console.log("C");
+// } else if (score >= 60) {
+//     console.log("D");
+// } else {
+//     console.log("F");
+// }
+
+// var x = [1, 2, 3, 4, 5, 6]
+// let sum = 0;
+
+// for (let i = 0; i<x.length; i++) {
+//     sum = sum + x[i];
+// }
+// console.log(sum);
+
+// let a = [12,34,56,1,75,3];
+// let num = 0;
+// for (let i = 0; i < a.length; i++) {
+//     if (a[i] > num) {
+//         num = a[i];
+//     }
+// }
+// console.log(num);
